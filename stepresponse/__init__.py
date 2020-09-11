d78bbe1ea1467348e5bf5e5dfde969082ec750c7
Sayan Das <47113876+AstroSayan@users.noreply.github.com>
2020-09-11 11:00:10 +0530

    Initialize the package

---
+++ stepresponse/__init__.py
@@ -0,0 +1,8 @@
+# __init__.py - initialization for control systems toolbox
+#
+# Author: Sayan Das
+# Date: 11 Sept 2020
+#
+# This file contains the initialization information from the control package.
+
+from .stepresponse import *
