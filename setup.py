53e33d81d16032eca8ff37b7f830f6d71d19dcc3
Sayan Das <47113876+AstroSayan@users.noreply.github.com>
2021-08-28 14:08:20 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="stepanalyser", 
-    version="0.2.0",
+    name="stepanalyzer",
+    version="0.3.0",
+    license='MIT',
     author="Sayan Das",
     author_email="astrosayan8@gmail.com",
-    description="Step response analyser",
+    description="Step Response analyzer",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/AstroSayan/StepAnalysis",
+    url="https://github.com/AstroSayan/StepAnalyzer",
+    download_url="https://github.com/AstroSayan/StepAnalyzer/archive/refs/tags/v0.3.0.tar.gz",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -20,6 +22,5 @@ setuptools.setup(
     ],
     python_requires='>=3.6',
     install_requires=['numpy',
-                      'control',
-                      'sympy'],
+                      'control', ],
 )
